6c9f17d71e0d52bc284ac6ec99e24df51258b4e6
Elijah Mixon <emixon98@gmail.com>
2026-03-07 20:16:46 -0800

    Formatting for Lab

---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+
+/*You will implement a HashTable class that stores:
+
+string keys
+int values
+You are NOT allowed to use:
+std::unordered_map
+std::map
+*/
+
+// Part 1
+class HashTable {
+    private:
+        vector<list<pair<string, int>>> table;
+        int currentSize;
+        int capacity;
+        int collisionCount;
+
+        int hashFunction(const string& key) const;
+        void rehash();
+
+    public:
+        HashTable(int size = 11);
+
+        void insert(const string& key, int value);
+        bool remove(const string& key);
+        int search(const string& key) const;
+        double loadFactor() const;
+        int size() const;
+        bool isEmpty() const;
+        void printTable() const;
+};
+
+// Part 2
+int HashTable::hashFunction(const string& key) const {
+    const int prime = 31;
+    long long hash = 0;
+
+    for (char c : key) {
+        hash = hash * prime + c;
+    }
+
+    return hash % capacity;
+}
+
+//Part 3 To-do
+//Use separate chaining (vector<list<>>)
+//If inserting into a non-empty bucket, increment collisionCount
+//If key already exists, update value instead of duplicating
+
+//Part 4 To -do
+/* 
+When: loadFactor() > 0.75
+You must:
+Double the table capacity
+Reinsert all existing elements
+Reset collision counter appropriately
+*/
+
+// Part 5
+
+/*
+In main():
+
+Insert at least 100 words
+Print:
+    Table capacity
+    Number of elements
+    Load factor
+    Total collisions
+Search for:
+    Existing key
+    Non-existing key
+    Remove some keys and verify correctness
+*/
+
+int main(){
+
+}
+
+
+/* Part 6
+Test three input types:
+Random strings
+Sequential keys (e.g., student1, student2, ...)
+Same prefix keys (e.g., data_0001, data_0002, ...)
+
+Record:
+Total collisions
+Maximum bucket size
+Average bucket length
+Write a short explanation (1--2 paragraphs) describing what you observe.
+
+*/
+//Using std::unordered_map results in major deduction
